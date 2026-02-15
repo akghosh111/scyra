9f6a4daecd426dc51542d4284b1da1177f9aee86
akghosh111 <ghoshkiran2017@gmail.com>
2026-02-15 15:04:44 +0530

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,10 @@
 import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../../prisma/generated/client"
 import { PrismaPg } from "@prisma/adapter-pg"
+import pg from "pg"
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-})
-
+const adapter = new PrismaPg(pg)
 const prisma = new PrismaClient({ adapter })
 
 export const auth = betterAuth({
